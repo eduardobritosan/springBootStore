c7a5318e5029faa56aba4ea3196ddde97e53aa3d
eduardo.brito <eebritosa@gmail.com>
2021-10-29 16:04:53 +0100

    Added JWT, currently passing deployment but need to switch login to React-Bootstrap.

--- src/main/java/com/eduardo/store/StoreApplication.java
+++ src/main/java/com/eduardo/store/StoreApplication.java
@@ -3,7 +3,10 @@ package com.eduardo.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaRepositories("com.eduardo.store.repo")
 @EntityScan("com.eduardo.store.model")
@@ -12,5 +15,15 @@ public class StoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:8082", "http://localhost:3000");
+			}
+		};
 	}
 }
---
+++ src/main/java/com/eduardo/store/service/IJwtUserDetailsService.java
@@ -0,0 +1,6 @@
+package com.eduardo.store.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface IJwtUserDetailsService extends UserDetailsService {
+}
