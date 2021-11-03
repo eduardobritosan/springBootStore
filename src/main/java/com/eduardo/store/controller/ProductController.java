0a996d406b871d1094ca8cc73cb49733160f45be
eduardo.brito <eebritosa@gmail.com>
2021-11-03 14:26:33 +0000

    Fixed CORS bug, deactivation working

--- src/main/java/com/eduardo/store/controller/ProductController.java
+++ src/main/java/com/eduardo/store/controller/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:8082"})
 @RestController
 @RequestMapping("/products")
 public class ProductController {
--- store-frontend/src/services/product.service.js
+++ store-frontend/src/services/product.service.js
@@ -1,13 +1,20 @@
 import axios from "axios";
+import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8082/products";
+
+const API_URL = "http://localhost:8082/products/";
 
 const getPublicContent = () => {
   return axios.get(API_URL);
 };
 
+const deactivate = (productCode) => {
+  return axios.patch(API_URL + `${productCode.productCode}`, {}, {headers: authHeader()})
+}
+
 const ProductService = {
-  getPublicContent
+  getPublicContent,
+  deactivate
 };
 
 export default ProductService;
