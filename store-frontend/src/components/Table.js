fe99e84a1abe1a425e130a4c6157ede8e01e0109
eduardo.brito <eebritosa@gmail.com>
2021-11-04 10:23:12 +0000

    Added Modal for details, pending Table refactoring.

--- store-frontend/src/components/Table.js
+++ store-frontend/src/components/Table.js
@@ -1,9 +1,17 @@
 import React, {useState, useEffect} from 'react'
+import {Modal} from 'react-bootstrap';
 import ProductService from "../services/product.service";
 
 
 const Table = () => {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [modalInfo, setModalInfo] = useState([]);
+  const [showModal, setShowModal] = useState([]);
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
 
   useEffect(() => {
     getData()
@@ -21,9 +29,70 @@ const Table = () => {
     })
   }
 
+  const details = (productCode) => {
+    ProductService.getProductByCode(productCode).then(res => {
+      setModalInfo(res.data);
+      toggleTrueFalse();
+    })
+  }
+
+  const toggleTrueFalse = () => {
+    setShowModal(handleShow);
+  }
+
+  const ModalContent = () => {
+    return (
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{modalInfo[0].productDescription}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Suppliers</h4>
+          <table id='product'>
+            <thead>
+              <tr>{renderSupplierHeader()}</tr>
+            </thead>
+            <tbody>
+              {renderSuppliers(modalInfo[0].suppliers)}
+            </tbody>
+          </table>
+          <h4>Price reductions</h4>
+          <table id='product'>
+            <thead>
+              <tr>{renderPriceRedHeader()}</tr>
+            </thead>
+            <tbody>
+              {renderPriceRed(modalInfo[0].priceReductions)}
+            </tbody>
+          </table>
+        </Modal.Body>
+        <Modal.Footer>
+        </Modal.Footer>
+      </Modal >
+    )
+  }
+
+  const renderSupplierHeader = () => {
+    let headerElement = ['name', 'country']
+
+    return headerElement.map((key, index) => {
+      return <th key={index}>{key.toUpperCase()}</th>
+    })
+  }
+
+  const renderPriceRedHeader = () => {
+    let headerElement = ['new price', 'start date', 'end date']
+
+    return headerElement.map((key, index) => {
+      return <th key={index}>{key.toUpperCase()}</th>
+    })
+  }
+
+
+
   const renderHeader = () => {
     let headerElement = ['name', 'price', 'status',
-      'created', 'creator', 'operation']
+      'created', 'creator', 'details', 'deactivate']
 
     return headerElement.map((key, index) => {
       return <th key={index}>{key.toUpperCase()}</th>
@@ -34,18 +103,45 @@ const Table = () => {
     return products && products.map(({productCode, productDescription, price,
       state, creationDate, creator}) => {
       return (
-        <tr key={productCode}>
+        <tr key={productCode} >
           <td>{productDescription}</td>
           <td>{price}</td>
           <td>{state}</td>
           <td>{new Date(creationDate).toLocaleDateString()}</td>
           <td>{creator}</td>
           <td className='opration'>
+            <button className='button' onClick={() => details(productCode)} >Details</button>
+          </td>
+          <td className='opration'>
             <button className='button' onClick={() => deactivate(productCode)} >Deactivate</button>
           </td>
+          {show ? <ModalContent /> : null}
         </tr >
       )
     })
+  }
+
+  const renderSuppliers = (suppliers) => {
+    return suppliers && suppliers.map(element => {
+      return (
+        <tr key={element.supplierDTO.supplierCode}>
+          <td>{element.supplierDTO.name}</td>
+          <td>{element.supplierDTO.country}</td>
+        </tr>
+      )
+    });
+  }
+
+  const renderPriceRed = (priceReductions) => {
+    return priceReductions && priceReductions.map(element => {
+      return (
+        <tr key={element.priceReductionCode}>
+          <td>{element.newPrice}</td>
+          <td>{new Date(element.startDate).toLocaleDateString()}</td>
+          <td>{new Date(element.endDate).toLocaleDateString()}</td>
+        </tr>
+      )
+    });
   }
 
   return (
--- store-frontend/src/services/product.service.js
+++ store-frontend/src/services/product.service.js
@@ -2,19 +2,24 @@ import axios from "axios";
 import authHeader from "./auth-header";
 
 
-const API_URL = "http://localhost:8082/products/";
+const API_URL = "http://localhost:8082/products";
 
 const getPublicContent = () => {
   return axios.get(API_URL);
 };
 
+const getProductByCode = (productCode) => {
+  return axios.get(API_URL + `?item-code=${productCode}`);
+};
+
 const deactivate = (productCode) => {
-  return axios.patch(API_URL + `${productCode.productCode}`, {}, {headers: authHeader()})
+  return axios.patch(API_URL + `/${productCode.productCode}`, {}, {headers: authHeader()})
 }
 
 const ProductService = {
   getPublicContent,
-  deactivate
+  deactivate,
+  getProductByCode
 };
 
 export default ProductService;
